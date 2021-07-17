9ac9624d086b5656b2748ba5d7cb8926349e2550
alireza jamshidi <71169797+alirezajam@users.noreply.github.com>
2021-07-17 13:29:44 +0430

    Update JamshidiTest.java

--- src/ir/batna/jamshidi/JamshidiTest.java
+++ src/ir/batna/jamshidi/JamshidiTest.java
@@ -1,4 +1,24 @@
-package ir.batna.jamshidi;
-
-public class JamshidiTest {
+import java.util.Scanner;
+class Main{
+    public static void main(String args[]){
+        int i,m=0,flag=0;
+        Scanner input = new Scanner ( System.in );
+        System.out.println("عدد مورد نظر را وارد کنید: ");
+        int n= input.nextInt ();//عدد گرفته شد از کاربر
+        i = 2;
+        m=n/2;
+        if(n==0||n==1){
+            System.out.println(n+" عدد اول نیست");
+        }else{
+            while(i<=m){
+                if(n%i==0){
+                    System.out.println(n+"عدد اول نیست");
+                    flag=1;
+                    break;
+                }
+                i++;
+            }
+            if(flag==0)  { System.out.println(n+" عدد اول است"); }
+        }//پایان else
+    }
 }
